8fcc3f6a195ea40a4fb8edc0a5a9f6c1fcce1ce2
awchen <awchen85@gmail.com>
2022-11-30 16:28:11 -0500

    added edit and delete cards

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,7 +1,7 @@
 import "./form";
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { initdb, getDb, postDb } from "./database";
+import { initdb, getDb, postDb, deleteDb, editDb } from "./database";
 import { fetchCards } from "./cards";
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
@@ -38,7 +38,13 @@ let profile = document.querySelector('input[type="radio"]:checked').value;
 if (submitBtnToUpdate == false) {
   postDb(name, email, phone, profile);
 } else {
-
+//Obtains values passed into the form element
+let name = document.getElementById('name').value;
+let phone = document.getElementById('phone').value;
+let email = document.getElementById('email').value;
+let profile = document.get.querySelector('input[type="radio]:checked').value;
+//Calls the editDB function passing in any values from the form element as well as the ID of the contact that we are updating
+editDb(profileId, name, email, phone, profile);
   fetchCards();
     // Toggles the submit button back to POST functionality
   submitBtnToUpdate = false;
@@ -51,6 +57,32 @@ toggleForm();
 // Reload the DOM
 fetchCards();
 });
-    // Import CSS files
-    import "../css/index.css";
+  // Import CSS files
+  import "../css/index.css";
+
+  window.deleteCard = (e) => {
+     //Grabs the id from the button element attached to the contact card.
+    let id = parseInt(e.id);
+    //Delete the card
+    deleteDb();
+    //Reload the DOM
+    fetchCards();
+  };
+  window.editCard = (e) => {
+    // Grabs the id from the button element attached to the contact card and sets a global variable that will be used in the form element
+    let profileId = parseInt(e.id);
+
+    let editName = e.dataset.name;
+    let editEmail = e.dataset.email;
+    let editPhone = e.dataset.phone;
+
+    document.getElementById('name').value = editName;
+    document.getElementById('email').value = editEmail;
+    document.getElementById('phone').value = editPhone;
+
+    form.style.display = "block";
+
+    //Toggles the Submit button so that it now Updates an existing contact instead of posting a new one
+    submitBtnToUpdate = true;
+  };
 
